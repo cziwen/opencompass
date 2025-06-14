fb34981cb9186cdbe11b7564ed1cf71660682dea
ziwenchen <zchen56@uw.edu>
2025-06-14 08:42:16 -0700

    updated

--- configs/my_eval/qwen2p5_1p5b_base_eval.py
+++ configs/my_eval/qwen2p5_1p5b_base_eval.py
@@ -39,64 +39,64 @@ datasets = [
         ),
         eval_cfg=default_eval_cfg,
     ),
-    # -------- 数据集 2：10K-modified ----------
-    dict (
-        type=JsonlDataset,
-        abbr='finqa10k_modified',
-        path='MyData/itzme091_financial-qa-10K-modified_alpaca.jsonl',
-        reader_cfg=dict (
-            input_columns=['instruction', 'input'],
-            output_column='output',
-        ),
-        infer_cfg=dict (  # 与上面同一套配置即可
-            prompt_template=dict (
-                type='PromptTemplate',
-                template=(
-                    '### Instruction:\n{instruction}\n\n'
-                    '### Input:\n{input}\n\n'
-                    '### Response:'
-                ),
-            ),
-            retriever=dict (type='ZeroRetriever'),
-            inferencer=dict (
-                type='GenInferencer',
-                max_out_len=512,
-                temperature=0.7,
-                top_p=0.95,
-                batch_size=32,
-            ),
-        ),
-        eval_cfg=default_eval_cfg,
-    ),
-    # -------- 数据集 3：10K-virattt ----------
-    dict (
-        type=JsonlDataset,
-        abbr='finqa10k_virattt',
-        path='MyData/virattt_financial_qa_10K_alpaca.jsonl',
-        reader_cfg=dict (
-            input_columns=['instruction', 'input'],
-            output_column='output',
-        ),
-        infer_cfg=dict (
-            prompt_template=dict (
-                type='PromptTemplate',
-                template=(
-                    '### Instruction:\n{instruction}\n\n'
-                    '### Input:\n{input}\n\n'
-                    '### Response:'
-                ),
-            ),
-            retriever=dict (type='ZeroRetriever'),
-            inferencer=dict (
-                type='GenInferencer',
-                max_out_len=512,
-                temperature=0.7,
-                top_p=0.95,
-                batch_size=32,
-            ),
-        ),
-        eval_cfg=default_eval_cfg,
-    ),
+    # # -------- 数据集 2：10K-modified ----------
+    # dict (
+    #     type=JsonlDataset,
+    #     abbr='finqa10k_modified',
+    #     path='MyData/itzme091_financial-qa-10K-modified_alpaca.jsonl',
+    #     reader_cfg=dict (
+    #         input_columns=['instruction', 'input'],
+    #         output_column='output',
+    #     ),
+    #     infer_cfg=dict (  # 与上面同一套配置即可
+    #         prompt_template=dict (
+    #             type='PromptTemplate',
+    #             template=(
+    #                 '### Instruction:\n{instruction}\n\n'
+    #                 '### Input:\n{input}\n\n'
+    #                 '### Response:'
+    #             ),
+    #         ),
+    #         retriever=dict (type='ZeroRetriever'),
+    #         inferencer=dict (
+    #             type='GenInferencer',
+    #             max_out_len=512,
+    #             temperature=0.7,
+    #             top_p=0.95,
+    #             batch_size=32,
+    #         ),
+    #     ),
+    #     eval_cfg=default_eval_cfg,
+    # ),
+    # # -------- 数据集 3：10K-virattt ----------
+    # dict (
+    #     type=JsonlDataset,
+    #     abbr='finqa10k_virattt',
+    #     path='MyData/virattt_financial_qa_10K_alpaca.jsonl',
+    #     reader_cfg=dict (
+    #         input_columns=['instruction', 'input'],
+    #         output_column='output',
+    #     ),
+    #     infer_cfg=dict (
+    #         prompt_template=dict (
+    #             type='PromptTemplate',
+    #             template=(
+    #                 '### Instruction:\n{instruction}\n\n'
+    #                 '### Input:\n{input}\n\n'
+    #                 '### Response:'
+    #             ),
+    #         ),
+    #         retriever=dict (type='ZeroRetriever'),
+    #         inferencer=dict (
+    #             type='GenInferencer',
+    #             max_out_len=512,
+    #             temperature=0.7,
+    #             top_p=0.95,
+    #             batch_size=32,
+    #         ),
+    #     ),
+    #     eval_cfg=default_eval_cfg,
+    # ),
 ]
 
 # ---------------- 3. 模型 ----------------
@@ -115,7 +115,7 @@ models = [
 
 # ---------------- 4. 推理 / 评测 Runner ----------------
 infer = dict(
-    partitioner=dict(type='NumWorkerPartitioner', num_worker=2),
+    partitioner=dict(type='NumWorkerPartitioner', num_worker=1),
     runner=dict(type='LocalRunner', task=dict(type='OpenICLInferTask')),
 )
 
